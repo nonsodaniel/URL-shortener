f73f7cac41c0abb96a7d06d85b84fcc6f5871031
nonsodaniel <nonsodaniel07@gmail.com>
2021-03-02 22:14:30 +0100

    Created application view

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
